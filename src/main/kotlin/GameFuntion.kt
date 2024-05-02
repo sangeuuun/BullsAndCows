855f1744b949d190f1cd0f65a9e1ab2df598a3da
sangeuuun <nwm21d@kakao.com>
2024-05-02 21:32:59 +0900

    class file 분리

---
+++ src/main/kotlin/GameFuntion.kt
@@ -0,0 +1,108 @@
+package org.example
+
+class GameFunction {
+    var gameHistory = mutableListOf<Int>()
+
+    fun start() {
+        while (true) {
+
+            val selectMenu = getMenuNum()
+            when (selectMenu) {
+                "1" -> startGame()
+                "2" -> viewGameHistory()
+                "3" -> {
+                    println("< 숫자 야구 게임을 종료합니다 >")
+                    return
+                }
+                else -> println("올바른 숫자를 입력해주세요!")
+            }
+        }
+    }
+    fun getMenuNum():String {
+        println("환영합니다! 원하시는 번호를 입력해주세요")
+        println("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기")
+
+        val menuInput = readln()
+
+        return menuInput
+    }
+
+    fun startGame() {
+        var cnt = 0
+        val answer = createAnswer().joinToString("")
+        println("")
+        println("< 게임을 시작합니다 >")
+        while (true) {
+            try {
+                println("숫자를 입력하세요")
+                val input = readln()
+
+                if (answer.length != input.toSet().size || input[0] == '0') {
+                    throw NumberFormatException()
+                }
+                cnt++
+                if (checkAnswer(answer, input)) break
+
+
+            } catch (e: NumberFormatException) {
+                println("올바르지 않은 입력값입니다")
+                println("")
+                continue
+            }
+        }
+        gameHistory.add(cnt)
+    }
+
+    fun createAnswer():MutableSet<Int> {
+        val answer = mutableSetOf<Int>()
+
+        while (answer.size < 3) {
+            val random = (0..9).random()
+            answer += random
+
+            if (answer.toList()[0] == 0) {
+                answer -= random
+            }
+        }
+        return answer
+    }
+
+    fun checkAnswer(answer:String, input:String): Boolean {
+        if (answer == input) {
+            println("정답입니다!")
+            println("")
+            return true
+        }
+
+        var strike = 0
+        var ball = 0
+
+        for (i in answer.indices) {
+            for (j in input.indices) {
+                if (answer[i] == input[j]) {
+                    if (i == j) strike++
+                    else ball++
+                }
+            }
+        }
+
+        if (strike == 0 && ball == 0) {
+            println("Nothing")
+            println("")
+            return false
+        }
+
+        println("${strike}스트라이크 ${ball}볼")
+        println("")
+        return false
+
+    }
+    fun viewGameHistory() {
+        println("")
+        println("< 게임 기록 보기 >")
+        if (gameHistory.isEmpty()) println("게임 기록이 없습니다.")
+        gameHistory.forEachIndexed { index, it -> println("${index+1}번째 게임 : 시도 횟수 - $it")}
+
+    }
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,55 +1,8 @@
 package org.example
 
 fun main() {
-    val n = "476"
-    println("< 게임을 시작합니다 >")
+    val game = GameFunction()
+    game.start()
 
-    while (true) {
-        try {
-            println("숫자를 입력하세요")
-            val input = readln().toInt()
-            val num = input.toString()
-
-            if (num.length != num.toSet().size || num.contains("0")) {
-                throw NumberFormatException()
-            }
-            if (n == num) {
-                println("정답입니다!")
-                break
-            }
-
-            var strike = 0
-            var ball = 0
-
-            for (i in n.indices) {
-                if (n[i] == num[i]) {
-                    strike++
-                }
-            }
-
-            for (i in n.indices) {
-                for (j in num.indices) {
-                    if (i != j && n[i] == num[j]) {
-                        ball++
-                    }
-                }
-            }
-
-            if (strike == 0 && ball == 0) {
-                println("Nothing")
-                println("")
-                continue
-            }
-
-            println("${strike}스트라이크 ${ball}볼")
-            println("")
-
-
-        } catch (e: NumberFormatException) {
-            println("올바르지 않은 입력값입니다")
-            println("")
-            continue
-        }
-    }
 
 }
